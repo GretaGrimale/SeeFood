8f86693c7ff8be693dfb1d0760026cffc58abf60
Greta Grimale <greta.gofmanaite@gmail.com>
2022-12-13 00:35:01 +0200

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Greta Grimale on 2022-12-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
